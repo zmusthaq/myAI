c783dc96ad360c74e21b62adab28458d831cd336
zmusthaq <122904666+zmusthaq@users.noreply.github.com>
2025-03-06 18:03:33 -0500

    changed message placeholder

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,7 +1,7 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Ask away!`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const MESSAGE_PLACEHOLDER: string = `Ask your questions here...`;
 export const FOOTER_MESSAGE: string = `Customized by Zach Musthaq UNC`;
 export const CLEAR_BUTTON_TEXT: string = `Clear chat`;
 export const PAGE_TITLE: string = `Investment Banking Interviewer`;
