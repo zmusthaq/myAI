ccd3a3e966a33e34fe56bebce474a458353c073a
zmusthaq <122904666+zmusthaq@users.noreply.github.com>
2025-03-06 17:50:14 -0500

    changed owner description

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Zach Musthaq's`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_DESCRIPTION: string = `Senior at UNC, future investment banker at Bank of America`;
 
 export const AI_NAME: string = `Lev`;
 export const AI_TONE: string = `[AI TONE]`;
