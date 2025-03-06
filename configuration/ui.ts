5e8f359ff2106017b8914842b4a28e678c586b8f
zmusthaq <122904666+zmusthaq@users.noreply.github.com>
2025-03-06 17:37:38 -0500

    changed page title to ib interviewer

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `Ask away!`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized by Zach Musthaq UNC`;
 export const CLEAR_BUTTON_TEXT: string = `Clear chat`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `Investment Banking Interviewer`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
