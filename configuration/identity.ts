6e2d434215b04d8e4e85f9a9064343933f23e02c
zmusthaq <122904666+zmusthaq@users.noreply.github.com>
2025-03-06 18:48:29 -0500

    changed AI tone

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Zach Musthaq`;
 export const OWNER_DESCRIPTION: string = `Senior at UNC, future investment banker at Bank of America`;
 
 export const AI_NAME: string = `Lev`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_TONE: string = `Technical`;
 export const AI_ROLE: string = `[AI ROLE]`;
