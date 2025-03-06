9c4f84f9b40de9f72c4b0513a3e81cd88fbe2adc
zmusthaq <122904666+zmusthaq@users.noreply.github.com>
2025-03-06 18:26:06 -0500

    updated footer

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -2,7 +2,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Ask away!`;
 export const MESSAGE_PLACEHOLDER: string = `Ask your questions here...`;
-export const FOOTER_MESSAGE: string = `Customized by Zach Musthaq UNC`;
+export const FOOTER_MESSAGE: string = `Customized by Zach Musthaq for UNC-BUSI488`;
 export const CLEAR_BUTTON_TEXT: string = `Clear chat`;
 export const PAGE_TITLE: string = `Investment Banking Interviewer`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, an AI chatbot to help you ace your investment banking interviews.`;
