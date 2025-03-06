23d2c91e09da0830e3063c54e6a580ebf466b268
zmusthaq <122904666+zmusthaq@users.noreply.github.com>
2025-03-06 17:44:42 -0500

    changed ai name

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Zach Musthaq's`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Lev`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
