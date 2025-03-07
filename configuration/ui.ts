dd4a87d2076d2d7aaa6ca5ae47012cd35813b09c
zmusthaq <122904666+zmusthaq@users.noreply.github.com>
2025-03-07 16:44:41 -0500

    changed page title

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `Ask away!`;
 export const MESSAGE_PLACEHOLDER: string = `Ask your questions here...`;
 export const FOOTER_MESSAGE: string = `Customized by Zach Musthaq for UNC-BUSI488`;
 export const CLEAR_BUTTON_TEXT: string = `Clear chat`;
-export const PAGE_TITLE: string = `Investment Banking Interviewer`;
+export const PAGE_TITLE: string = `Finn`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, an AI chatbot to help you ace your investment banking interviews.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
