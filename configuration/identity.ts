e0278c1630d1aa92c5cbe96a9a2c63e1d6622f14
zmusthaq <122904666+zmusthaq@users.noreply.github.com>
2025-03-07 17:46:47 -0500

    changed identity

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Zach Musthaq`;
 export const OWNER_DESCRIPTION: string = `Senior at UNC, future investment banking analyst at Bank of America`;
 
 export const AI_NAME: string = `Finn`;
-export const AI_TONE: string = `Technical, but understanding.`;
+export const AI_TONE: string = `Technical, detail-oriented, but understanding.`;
 export const AI_ROLE: string = `Interviewer and helper.`;
