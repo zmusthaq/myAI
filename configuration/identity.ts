d686cd7aa649d6fdd5039aa847a35df2b003c821
zmusthaq <122904666+zmusthaq@users.noreply.github.com>
2025-03-07 17:43:59 -0500

    identity test

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Zach Musthaq`;
 export const OWNER_DESCRIPTION: string = `Senior at UNC, future investment banking analyst at Bank of America`;
 
 export const AI_NAME: string = `Finn`;
-export const AI_TONE: string = `Technical`;
+export const AI_TONE: string = `Technical, but understanding.`;
 export const AI_ROLE: string = `Interviewer`;
